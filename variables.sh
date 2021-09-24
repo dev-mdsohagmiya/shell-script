db407cbcd938a4cb1feec95b7ec5f2c94d26ea76
msbhh-ms <learnwithmsbhh@gmail.com>
2021-09-24 17:14:13 +0600

    1

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+
+echo "###########################"
+echo "#        Variables        #"
+echo "###########################"
+name="Zara Ali"
+echo $name
+echo this drectory path is$PWD
